b4c11e0c61b4fce0b448cde9fcc387f441532225
CintiaBEC <110127199+CintiaBEC@users.noreply.github.com>
2025-05-10 19:27:04 -0300

    nuevo ejercicio de práctica

---
+++ pract_alura.py
@@ -0,0 +1,3 @@
+#print('Cantidad\tCalidad\n5 muestras\tAlta\n3 muestras\tBaja')
+#print("Ruta del archivo: C:\\archivos\\documento.csv")
+print("Una vez oí: \"Los frutos del conocimiento son los más dulces y duraderos de todos.\"")
